6833fc5110f4128aacba53dd58156dda5c81e0dc
BalbesKiryusha43 <118163485+BalbesKiryusha43@users.noreply.github.com>
2022-11-18 23:44:48 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,16 @@
+﻿) begin
+var A: array of integer;
+var N: integer;
+var k: integer;
+  write('Введите размер массива:');
+  read (N);
+  SetLength ( A, N);
+  for var i := 0 to N-1 do
+    readln (A[i]);
+  for var i := 2 to N-1 do 
+    if a[i]>=a[i-1] then k+=1;
+ if k = 0 then writeln ('Заданный массив является убывающим. ')
+ else
+ println ('Последовательность не убывает либо в массиве есть одинаковые элементы!');
+  SetLength(A, 0);
+end.
---
+++ 2.pas
@@ -0,0 +1,29 @@
+﻿Begin
+var  s:= ReadString('   Строка: ');
+var ss:= ReadString('Подстрока: ');
+var  i, j, p, q: Integer;
+var  t: Char;
+  i:= 1;
+  j:= Length(s)-Length(ss)+1;
+  while i <= j do
+    begin
+    if s[i:i+length(ss)]=ss then
+      begin
+        p:= i;
+        i+=Length(ss);
+        q:=i-1;
+        while p < q do
+          begin
+            t:=s[p];
+            s[p]:= s[q];
+            s[q]:= t;
+            p+=1;
+            q-=1;
+          end;
+        end
+      else 
+        i+=1;
+  end;
+  Print('Результат: ');
+  PrintLn(s);
+end.
